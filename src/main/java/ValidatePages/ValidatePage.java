4bf6850755f4fe1e2db9bf2f09cef1e988d06e6a
Saurabh18122001 <landesaurabh2001@gmail.com>
2025-11-25 10:36:35 +0530

    new add

--- src/main/java/ValidatePages/ValidatePage.java
+++ src/main/java/ValidatePages/ValidatePage.java
@@ -1,12 +1,14 @@
 package ValidatePages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class ValidatePage {
 
 	public static void main(String[] args) {
 		System.out.println("hii");
-		WebDriver driver = new FirefoxDriver();
+		WebDriver driver = new ChromeDriver();
+		driver.close();
 	}
 }
